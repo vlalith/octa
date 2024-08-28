98054c318007c5d52ebf80c4b647459b0e7fbc54
vlalith <132739985+vlalith@users.noreply.github.com>
2024-08-28 13:31:20 +0530

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,9 @@
+// Smooth Scrolling
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
